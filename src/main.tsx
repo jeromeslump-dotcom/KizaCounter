ea52f017e32d0bfde10dd5a345877c6bd2f8b3ab
jeromeslump-dotcom <jeromeslump@gmail.com>
2026-09-12 16:11:54 +0200

    Load centralized semantic UI styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthSessionProvider } from "./auth/useAuthSession";
 import "./index.css";
+import "./ui/semantic.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
