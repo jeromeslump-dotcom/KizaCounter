5c4aa90d52e5ac6124ea5994e98ef978e735078d
jeromeslump-dotcom <jeromeslump@gmail.com>
2026-09-12 16:00:42 +0200

    Centralize CounterModal recommendation panel style

--- src/components/CounterModal.tsx
+++ src/components/CounterModal.tsx
@@ -435,7 +435,7 @@ export default function CounterModal({
           </div>
 
           {hasRecommendations && (
-            <div className="ui-recommendations mt-4 rounded-xl border p-2 sm:p-3">
+            <div className="ui-panel-alt mt-4 rounded-xl border p-2 sm:p-3">
               {recommendedTeam.length > 0 && (
                 <button
                   type="button"
